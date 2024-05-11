552a9c6db44ffa0faddb9918f0d4c811b0c7fff2
Tünzalə <tunzale@Tuni-MacBook-Air.local>
2024-05-12 02:07:28 +0400

    added task file

---
+++ OneToMany-task/wwwroot/js/blog.js
@@ -0,0 +1,25 @@
+﻿
+$(function () {
+
+    $(document).on("click", ".show-more button", function () {
+        let skip = parseInt($(".blogs-area").children().length);
+
+        let parentElem = $(".blogs-area");
+
+        let parentElemContent = $(".blogs-area").html();
+
+        $.ajax({
+            url: `blog/showmore?skip=$(skip)`,
+            type: "GET",
+            success: function (response) {
+                parentElemContent += response;
+                $(response).html(parentElemContent);
+
+                console.log(response)
+            },
+        });
+    })
+
+
+
+})
